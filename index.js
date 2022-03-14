2e4aad22625defdcd360205ca0eed9e06dbfcfe7
swetapatel27 <sweta.patel27@gmail.com>
2022-03-14 11:08:04 +0530

    cors added

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var mongoose = require('mongoose')
 var express = require('express')
 var route = require('./routes')
 var bodyParser =require('body-parser')
+const cors = require("cors");
 mongoose.connect('mongodb+srv://studentdb:studentdb123@mycluster.jtfqi.mongodb.net/Student?retryWrites=true&w=majority').then(()=>{
     console.log('connected')
 
     app = express();
+    app.use(cors());
     app.use(bodyParser.urlencoded({extended:false}))
     app.use('/api',route)
     
